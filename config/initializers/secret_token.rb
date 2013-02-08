54b71dc55d735d454ad511aa0de111fc691634c7
Zain Aziz <zain.aziz76@gmail.com>
2013-02-07 21:57:00 -0800

    Added Status Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heubook::Application.config.secret_token = 'ad411f369604dce23b858f35f69dc3d612ad5bdb9db928aafea40da517e5ef000973ffc7daf825bf422d73778e51c0434bc00d34fbe960b5514ec93e1545451a'
